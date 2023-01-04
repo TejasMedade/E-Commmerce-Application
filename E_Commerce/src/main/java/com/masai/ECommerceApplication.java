91aea8f3547592e6aa8c0107fb98562a91a75b8e
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-01-04 19:15:32 +0530

    #Added Model Mapper

--- E_Commerce/src/main/java/com/masai/ECommerceApplication.java
+++ E_Commerce/src/main/java/com/masai/ECommerceApplication.java
@@ -1,13 +1,26 @@
 package com.masai;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ECommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+	// Model Mapper Bean
+	@Bean
+	public ModelMapper modelMapper() {
+
+		return new ModelMapper();
+
 	}
 
 }
