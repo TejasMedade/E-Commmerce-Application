0113b4323cc176ffb101ed7f94c667205810e539
Tejas Vilas Medade <tejasmedade@gmail.com>
2023-01-11 23:40:19 +0530

    Update Category.java

--- E_Commerce/src/main/java/com/masai/model/Category.java
+++ E_Commerce/src/main/java/com/masai/model/Category.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -37,7 +38,8 @@ public class Category {
 	private String categoryName;
 
 	private String subCategory;
-
+	
+	@Column(length = Integer.MAX_VALUE)
 	private String categoryDescription;
 
 	private Boolean active;
