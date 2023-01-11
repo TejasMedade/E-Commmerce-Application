c5ff172be23a006afe1ff5f4b2f7b5d6068605e3
Tejas Vilas Medade <tejasmedade@gmail.com>
2023-01-11 23:40:25 +0530

    Update Customer.java

--- E_Commerce/src/main/java/com/masai/model/Customer.java
+++ E_Commerce/src/main/java/com/masai/model/Customer.java
@@ -4,6 +4,8 @@
 package com.masai.model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -11,14 +13,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.Past;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
 
 /**
  * @author tejas
@@ -34,25 +35,33 @@ public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer customerId;
-	
+
 	private String firstName;
-	
+
 	private String lastName;
-	
-	@Column(unique=true)
+
+	@Column(unique = true)
 	private String contact;
-	
+
 	private String email;
-	
+
 	private String password;
-	
+
 	@Past
 	private LocalDate dateOfBirth;
-	
+
 	private LocalDate accountCreatedDate;
-	
+
 	@OneToOne(cascade = CascadeType.ALL)
 	private Address address;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private Cart cart;
+
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Review> listOfReviews = new ArrayList<>();
 	
-	
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Order> listOfOrders = new ArrayList<>();
+
 }
