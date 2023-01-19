d9a893888e99514c843163741cdec58a4f9c25a9
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-01-19 22:31:18 +0530

    Create ReviewController.java

---
+++ E_Commerce/src/main/java/com/masai/controllers/ReviewController.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.masai.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tejas
+ *
+ */
+@RestController
+@RequestMapping("bestbuy/reviews")
+public class ReviewController {
+
+}
