63093909ec71cf2f3383f7cd615252cdec3d1c18
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-01-21 19:13:42 +0530

    Create ResourceNotAllowedException.java

---
+++ E_Commerce/src/main/java/com/masai/exceptions/ResourceNotAllowedException.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.masai.exceptions;
+
+/**
+ * @author tejas
+ *
+ */
+public class ResourceNotAllowedException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+
+	String resourceName;
+	String fieldName;
+	String fieldValue;
+	Integer fieldValue2;
+
+	/**
+	 * @param resourceName
+	 * @param fieldName
+	 * @param userId
+	 */
+
+	//Payment Method Associated With This Payment Id is Temporarily Unavailable !
+	
+	
+	public ResourceNotAllowedException(String resourceName,String fieldName, Integer fieldValue2) {
+		super(String.format("%s Associated With This %s : %s is Temporarily Unavailable !", resourceName,fieldName, fieldValue2));
+		this.resourceName = resourceName;
+		this.fieldName = fieldName;
+		this.fieldValue2 = fieldValue2;
+	}
+	
+	
+	
+
+	
+	
+	
+
+	
+
+}
