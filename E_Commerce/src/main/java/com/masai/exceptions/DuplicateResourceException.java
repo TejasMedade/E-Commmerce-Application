7ef6f148303dfa235108f66379d93a752cf52e3c
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-01-21 19:13:29 +0530

    Update DuplicateResourceException.java

--- E_Commerce/src/main/java/com/masai/exceptions/DuplicateResourceException.java
+++ E_Commerce/src/main/java/com/masai/exceptions/DuplicateResourceException.java
@@ -17,17 +17,32 @@ public class DuplicateResourceException extends Exception {
 	String resourceName;
 	String fieldName;
 	String fieldValue;
+	Integer fieldValue2;
 
 	/**
 	 * @param resourceName
 	 * @param fieldName
 	 * @param userId
 	 */
-	public DuplicateResourceException(String resourceName, String fieldName, String userId) {
-		super(String.format("Error : %s Already Registered With This %s : %s", resourceName, fieldName, userId));
+	public DuplicateResourceException(String resourceName, String fieldName, String fieldValue) {
+		super(String.format("Error : %s Already Registered With This %s : %s", resourceName, fieldName, fieldValue));
 		this.resourceName = resourceName;
 		this.fieldName = fieldName;
-		this.fieldValue = userId;
+		this.fieldValue = fieldValue;
 	}
+
+	/**
+	 * @param resourceName
+	 * @param fieldName
+	 * @param fieldValue2
+	 */
+	public DuplicateResourceException(String resourceName, String fieldName, Integer fieldValue2) {
+		super(String.format("Error : Another %s Method is Already Registered With This %s : %s", resourceName, fieldName, fieldValue2));
+		this.resourceName = resourceName;
+		this.fieldName = fieldName;
+		this.fieldValue2 = fieldValue2;
+	}
+	
+	
 
 }
