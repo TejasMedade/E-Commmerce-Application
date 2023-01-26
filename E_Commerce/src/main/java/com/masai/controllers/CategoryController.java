45c7f9f14673987f32afea46139069ed12576027
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-01-27 03:54:49 +0530

    Update CategoryController.java

--- E_Commerce/src/main/java/com/masai/controllers/CategoryController.java
+++ E_Commerce/src/main/java/com/masai/controllers/CategoryController.java
@@ -3,11 +3,19 @@
  */
 package com.masai.controllers;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,13 +27,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.masai.exceptions.ResourceNotFoundException;
+import com.masai.model.Category;
 import com.masai.modelRequestDto.CategoryRequestDto;
 import com.masai.modelRequestDto.CategoryUpdateRequestDto;
 import com.masai.modelResponseDto.CategoryResponseDto;
 import com.masai.payloads.ApiResponse;
 import com.masai.payloads.AppConstants;
-import com.masai.payloads.PageResponse;
+import com.masai.payloads.CategoryModelAssembler;
 import com.masai.services.CategoryServices;
 
 /**
@@ -39,6 +49,12 @@ public class CategoryController {
 	@Autowired
 	private CategoryServices categoryServices;
 
+	@Autowired
+	private CategoryModelAssembler categoryModelAssembler;
+
+	@Autowired
+	private PagedResourcesAssembler<Category> pagedResourcesAssembler;
+
 	@GetMapping("/all")
 	public ResponseEntity<List<CategoryResponseDto>> getAllCategoriesHandler() {
 
@@ -48,20 +64,39 @@ public class CategoryController {
 	}
 
 	@PostMapping("/")
-	public ResponseEntity<CategoryResponseDto> addCategoryHandler(@Valid @RequestBody CategoryRequestDto categoryRequestDto) {
+	public ResponseEntity<CategoryResponseDto> addCategoryHandler(
+			@Valid @RequestBody CategoryRequestDto categoryRequestDto) throws ResourceNotFoundException {
 
-		CategoryResponseDto category = this.categoryServices.addCategory(categoryRequestDto);
+		CategoryResponseDto categoryResponseDto = this.categoryServices.addCategory(categoryRequestDto);
 
-		return new ResponseEntity<CategoryResponseDto>(category, HttpStatus.ACCEPTED);
+		// Self Link
+		categoryResponseDto
+				.add(linkTo(methodOn(CategoryController.class).getCategoryHandler(categoryResponseDto.getCategoryId()))
+						.withSelfRel());
+
+		// Collection Link
+		categoryResponseDto.add(linkTo(methodOn(CategoryController.class).getAllCategoriesByPageHandler(null, null))
+				.withRel(IanaLinkRelations.COLLECTION));
+
+		return new ResponseEntity<CategoryResponseDto>(categoryResponseDto, HttpStatus.CREATED);
 	}
 
 	@PutMapping("/{categoryId}/update")
 	public ResponseEntity<CategoryResponseDto> updateCategoryHandler(@PathVariable("categoryId") Integer categoryId,
 			@Valid @RequestBody CategoryUpdateRequestDto categoryRequestDto) throws ResourceNotFoundException {
 
-		CategoryResponseDto updatedCategory = this.categoryServices.updateCategory(categoryId, categoryRequestDto);
+		CategoryResponseDto categoryResponseDto = this.categoryServices.updateCategory(categoryId, categoryRequestDto);
 
-		return new ResponseEntity<CategoryResponseDto>(updatedCategory, HttpStatus.ACCEPTED);
+		// Self Link
+		categoryResponseDto
+				.add(linkTo(methodOn(CategoryController.class).getCategoryHandler(categoryResponseDto.getCategoryId()))
+						.withSelfRel());
+
+		// Collection Link
+		categoryResponseDto.add(linkTo(methodOn(CategoryController.class).getAllCategoriesByPageHandler(null, null))
+				.withRel(IanaLinkRelations.COLLECTION));
+
+		return new ResponseEntity<CategoryResponseDto>(categoryResponseDto, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/{categoryId}/delete")
@@ -77,32 +112,55 @@ public class CategoryController {
 	public ResponseEntity<CategoryResponseDto> getCategoryHandler(@PathVariable("categoryId") Integer categoryId)
 			throws ResourceNotFoundException {
 
-		CategoryResponseDto category = this.categoryServices.getCategory(categoryId);
+		CategoryResponseDto categoryResponseDto = this.categoryServices.getCategory(categoryId);
 
-		return new ResponseEntity<CategoryResponseDto>(category, HttpStatus.FOUND);
+		// Self Link
+		categoryResponseDto
+				.add(linkTo(methodOn(CategoryController.class).getCategoryHandler(categoryResponseDto.getCategoryId()))
+						.withSelfRel());
+
+		// Collection Link
+		categoryResponseDto.add(linkTo(methodOn(CategoryController.class).getAllCategoriesByPageHandler(null, null))
+				.withRel(IanaLinkRelations.COLLECTION));
+
+		return new ResponseEntity<CategoryResponseDto>(categoryResponseDto, HttpStatus.OK);
 	}
 
 	@GetMapping("/all/page")
-	public ResponseEntity<PageResponse> getAllCategoriesByPageHandler(
+	public ResponseEntity<CollectionModel<CategoryResponseDto>> getAllCategoriesByPageHandler(
 			@RequestParam(defaultValue = AppConstants.PAGENUMBER, required = false) Integer pageNumber,
 			@RequestParam(defaultValue = AppConstants.PAGESIZE, required = false) Integer pageSize) {
 
-		PageResponse categoriesByPage = this.categoryServices.getAllCategoriesByPage(pageNumber, pageSize);
+		Page<Category> pageResponse = this.categoryServices.getAllCategoriesByPage(pageNumber, pageSize);
 
-		return new ResponseEntity<PageResponse>(categoriesByPage, HttpStatus.OK);
+		PagedModel<CategoryResponseDto> model = this.pagedResourcesAssembler.toModel(pageResponse,
+				categoryModelAssembler);
+
+		// Collection Link
+		model.add(linkTo(methodOn(CategoryController.class).getAllCategoriesByPageHandler(null, null))
+				.withRel(IanaLinkRelations.COLLECTION));
+
+		return new ResponseEntity<CollectionModel<CategoryResponseDto>>(model, HttpStatus.OK);
 	}
 
 	@GetMapping("/all/sortby")
-	public ResponseEntity<PageResponse> getSortedByAnyCategoryDetailsByPageHandler(
+	public ResponseEntity<CollectionModel<CategoryResponseDto>> getSortedByAnyCategoryDetailsByPageHandler(
 			@RequestParam(defaultValue = AppConstants.PAGENUMBER, required = false) Integer pageNumber,
 			@RequestParam(defaultValue = AppConstants.PAGESIZE, required = false) Integer pageSize,
 			@RequestParam(defaultValue = AppConstants.CATEGORYSORTBY, required = false) String sortBy,
 			@RequestParam(defaultValue = AppConstants.SORTDIRECTION, required = false) String sortDirection) {
 
-		PageResponse categoriesByPage = this.categoryServices.getSortedByAnyCategoryDetailsByPage(pageNumber, pageSize,
+		Page<Category> pageResponse = this.categoryServices.getSortedByAnyCategoryDetailsByPage(pageNumber, pageSize,
 				sortBy, sortDirection);
 
-		return new ResponseEntity<PageResponse>(categoriesByPage, HttpStatus.OK);
+		PagedModel<CategoryResponseDto> model = this.pagedResourcesAssembler.toModel(pageResponse,
+				categoryModelAssembler);
+
+		// Collection Link
+		model.add(linkTo(methodOn(CategoryController.class).getAllCategoriesByPageHandler(null, null))
+				.withRel(IanaLinkRelations.COLLECTION));
+
+		return new ResponseEntity<CollectionModel<CategoryResponseDto>>(model, HttpStatus.OK);
 
 	}
 
@@ -112,7 +170,7 @@ public class CategoryController {
 
 		List<CategoryResponseDto> categoriesByKeyword = this.categoryServices.searchCategoriesByKeyword(keyword);
 
-		return new ResponseEntity<List<CategoryResponseDto>>(categoriesByKeyword, HttpStatus.FOUND);
+		return new ResponseEntity<List<CategoryResponseDto>>(categoriesByKeyword, HttpStatus.OK);
 	}
 
 }
