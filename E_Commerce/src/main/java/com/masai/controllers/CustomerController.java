6d118550d4ddc4144ba7ffea7732520651dc970c
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-01-31 21:57:49 +0530

    Update CustomerController.java

--- E_Commerce/src/main/java/com/masai/controllers/CustomerController.java
+++ E_Commerce/src/main/java/com/masai/controllers/CustomerController.java
@@ -18,6 +18,7 @@ import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -131,6 +132,7 @@ public class CustomerController {
 		return new ResponseEntity<CustomerDetailsResponseDto>(updateCustomerDetails, HttpStatus.OK);
 	}
 
+	@PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
 	@DeleteMapping("/{contact}/delete")
 	public ResponseEntity<ApiResponse> deleteCustomerAccountHandler(@PathVariable("contact") String contact)
 			throws ResourceNotFoundException {
