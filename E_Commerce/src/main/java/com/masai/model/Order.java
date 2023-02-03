707e7ed7183b3791c5fb92ba92e023e44111e224
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-02-04 00:09:04 +0530

    Update Order.java

--- E_Commerce/src/main/java/com/masai/model/Order.java
+++ E_Commerce/src/main/java/com/masai/model/Order.java
@@ -3,19 +3,21 @@
  */
 package com.masai.model;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -30,24 +32,62 @@ import lombok.NoArgsConstructor;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-public class Order {
+public class Order  {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer orderId;
 
-	private LocalDateTime orderDateTime;
+	// Ordered //Dispatched //Shipped //Delivered //Cancelled //
+	private String orderStatus;
+	
+	@CreationTimestamp
+	@Column(nullable = false, updatable = false)	
+	private LocalDateTime orderTimeStamp;
+	
+	@UpdateTimestamp
+	@Column(nullable = false)	
+	private LocalDateTime orderUpdatedTimeStamp;
+	
+	private LocalDateTime expectedDeliveryDate;
 
-	private LocalDate expectedDeliveryDate;
+	private Integer orderQuantity;
 
 	private Double orderTotalAmount;
-
-	@ManyToOne
-	private Customer customer;
 
 	@OneToOne
 	private Payment payment;
 
-	@OneToMany(cascade = CascadeType.ALL)
-	private List<OrderDetails> listOfOrderDetails = new ArrayList<>();
+	@ManyToOne
+	private Customer customer;
+
+	@ElementCollection
+	private List<OrderProductDetails> listOfProducts = new ArrayList<>();
+
+	// Cancelled By Customer Product Should be added to Inventory
+	private Boolean isOrderCancelled;
+
+	// Returned By Customer Return Request
+	// Should be Generated, After Confirming Pickup request should be generated,
+	// after pickup, refund request or replace request should be generated Product
+	// Should be added to Inventory,
+	private Boolean isOrderReturned;
+
+	// Replaced By Customer Product Should be Not be added to Inventory, Replacement
+	// Request Should be Generated, After Confirming New Order Should be Placed
+	private Boolean isOrderReplaced;
+
+	// Refunded By BestBuy Product Should be added to Inventory, Refund Request
+	// Should be Generated, And Payment Should be recieved by the Customer
+	private Boolean isOrderRefunded;
+
+	private Boolean isOrderDelievered;
+
+	// Replacement Order
+	private Boolean isReplacementOrder;
+
+	// Replacement Order for Original Order
+	private Integer orginialOrderId;
+
+
 }
