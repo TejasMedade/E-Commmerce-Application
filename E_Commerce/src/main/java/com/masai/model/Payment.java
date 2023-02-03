3767dd26bc2c9e33a13b2ebe33bfc11ce6af27f3
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-02-04 00:09:12 +0530

    Update Payment.java

--- E_Commerce/src/main/java/com/masai/model/Payment.java
+++ E_Commerce/src/main/java/com/masai/model/Payment.java
@@ -3,8 +3,16 @@
  */
 package com.masai.model;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -21,10 +29,18 @@ import lombok.NoArgsConstructor;
 public class Payment {
 
 	@Id
+	@Column(unique = true)
 	private Integer paymentId;
 
-	private Boolean paymentType;
+	private String paymentType;
 
 	private Boolean allowed;
-
+	
+	@CreationTimestamp
+	@Column(nullable = false,updatable = false)
+	private LocalDateTime paymentAddedTimeStamp;
+	
+	@UpdateTimestamp
+	@Column(nullable = false)
+	private LocalDateTime paymentUpdatedTimeStamp;
 }
