958f92af21cec896a93089b11fccb9fd5d28c909
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-02-04 00:09:19 +0530

    Create PickUpOrderRequest.java

---
+++ E_Commerce/src/main/java/com/masai/model/PickUpOrderRequest.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.masai.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author tejas
+ *
+ */
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class PickUpOrderRequest {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer pickUpOrderRequestId;
+
+	@CreationTimestamp
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime pickUpOrderTimeStamp;
+
+	@UpdateTimestamp
+	@Column(nullable = false)
+	private LocalDateTime pickUpOrderUpdatedTimeStamp;
+
+	private LocalDateTime expectedPickUpDate;
+
+	private String pickedUpBy;
+
+	private Boolean isReturnOrderPickedUp;
+
+	@OneToOne
+	private Customer customer;
+
+	@OneToOne
+	private Order order;
+
+}
