9442122220a617c87f7ad451d4f98bda563dfcc6
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-02-05 15:10:46 +0530

    Update AdminServices.java

--- E_Commerce/src/main/java/com/masai/services/AdminServices.java
+++ E_Commerce/src/main/java/com/masai/services/AdminServices.java
@@ -3,19 +3,30 @@
  */
 package com.masai.services;
 
+import java.util.List;
+
+import com.masai.exceptions.ResourceNotFoundException;
+import com.masai.modelRequestDto.AdminRequestDto;
+import com.masai.modelRequestDto.AdminUpdateRequestDto;
+import com.masai.modelResponseDto.AdminResponseDto;
+import com.masai.payloads.ApiResponse;
+
 /**
  * @author tejas
  *
  */
 public interface AdminServices {
 
-	// Do With JWT
-	void signUpAdmin();
+	AdminResponseDto registerAdmin(AdminRequestDto adminRequestDto) throws ResourceNotFoundException;
 
-	// Do With JWT
-	void udpateAdminDetails();
+	AdminResponseDto updateAdminDetails(AdminUpdateRequestDto userdto, Integer adminId)
+			throws ResourceNotFoundException;
 
-	// Do With JWT
-	void deleteAdminAccount();
+	AdminResponseDto getAdminDetailsById(Integer adminId) throws ResourceNotFoundException;
 
+	List<AdminResponseDto> getAllAdmins();
+
+	ApiResponse deleteAdminById(Integer adminId) throws ResourceNotFoundException;
+	
+	
 }
