86505a9592eb717e8498537ffdfc02b7fd7c9717
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-02-05 15:10:53 +0530

    Update CustomerServices.java

--- E_Commerce/src/main/java/com/masai/services/CustomerServices.java
+++ E_Commerce/src/main/java/com/masai/services/CustomerServices.java
@@ -3,9 +3,17 @@
  */
 package com.masai.services;
 
+import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.data.domain.Page;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.masai.exceptions.FileTypeNotValidException;
 import com.masai.exceptions.ResourceNotFoundException;
+import com.masai.model.Customer;
 import com.masai.modelRequestDto.CustomerRequestDto;
 import com.masai.modelRequestDto.CustomerUpdateRequestDto;
 import com.masai.modelResponseDto.CustomerDetailsResponseDto;
@@ -19,15 +27,31 @@ import com.masai.payloads.ApiResponse;
 
 public interface CustomerServices {
 
-	public CustomerDetailsResponseDto signUpCustomer(CustomerRequestDto customerRequestDto);
+	CustomerDetailsResponseDto registerCustomer(CustomerRequestDto customerRequestDto) throws ResourceNotFoundException;
 
-	public CustomerDetailsResponseDto updateCustomerDetails(String contact,
-			CustomerUpdateRequestDto customerUpdateRequestDto) throws ResourceNotFoundException;
+	CustomerDetailsResponseDto updateCustomerDetails(String contact, CustomerUpdateRequestDto customerUpdateRequestDto)
+			throws ResourceNotFoundException;
 
-	public ApiResponse deleteCustomerAccount(String contact) throws ResourceNotFoundException;
+	ApiResponse deleteCustomerAccount(String contact) throws ResourceNotFoundException;
 
-	public List<CustomerResponseDto> getAllCustomerDetails();
+	CustomerResponseDto getCustomer(String contact) throws ResourceNotFoundException;
 
-	public CustomerResponseDto getCustomer(String contact) throws ResourceNotFoundException;
+	CustomerDetailsResponseDto updateCustomerImage(String contact, MultipartFile image)
+			throws ResourceNotFoundException, IOException, FileTypeNotValidException;
+
+	void serveCustomerImage(String imageName, HttpServletResponse response)
+			throws ResourceNotFoundException, IOException, FileTypeNotValidException;
+
+	ApiResponse deleteCustomerImage(String fileName) throws IOException;
+
+	List<CustomerResponseDto> searchByfirstName(String firstName);
+
+	Page<Customer> getAllCustomerDetails(Integer page, Integer size, String sortDirection, String sortBy);
+
+	List<CustomerResponseDto> searchBylastName(String lastName);
+
+	List<CustomerResponseDto> searchByemailId(String email);
+
+	List<CustomerResponseDto> searchByFirstAndLastName(String firstName, String lastName);
 
 }
