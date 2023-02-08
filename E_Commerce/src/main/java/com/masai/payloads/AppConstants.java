290f903e10f0336b24c673de718217fbd3002bc2
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-02-08 22:45:52 +0530

    Update AppConstants.java

--- E_Commerce/src/main/java/com/masai/payloads/AppConstants.java
+++ E_Commerce/src/main/java/com/masai/payloads/AppConstants.java
@@ -2,10 +2,19 @@ package com.masai.payloads;
 
 public class AppConstants {
 
+	public static final String QUANTITY = "1";
 	public static final String PAGENUMBER = "0";
 	public static final String PAGESIZE = "5";
-	public static final String SORTBY = "productId";
+	public static final String PRODUCTSORTBY = "productId";
+	public static final String CATEGORYSORTBY = "categoryId";
 	public static final String SORTDIRECTION = "ASC";
+	public static final String RATINGSORTDIRECTION = "DESC";
+	public static final String CUSTOMERSORTBY = "userId";
+	public static final String FEEDBACKSORTBY = "feedbackId";
+	public static final String ORDERSORTBY = "orderId";
+	public static final String REFUNDORDERSORTBY = "refundOrderRequestId";
+	public static final String REPLACEORDERSORTBY = "replaceOrderRequestId";
+	public static final String PICKUPORDERSORTBY = "pickUpOrderRequestId";
 	public static final Integer ROLE_ADMIN = 1001;
 	public static final Integer ROLE_USER = 1002;
 
