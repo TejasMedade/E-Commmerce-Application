13ab59eba29b6d2180a3d0c36a36e09d3671efc1
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-02-09 23:20:59 +0530

    Create RoleResponseDto.java

---
+++ E_Commerce/src/main/java/com/masai/modelResponseDto/RoleResponseDto.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.masai.modelResponseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author tejas
+ *
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleResponseDto {
+
+	private Integer roleId;
+
+	private String roleName;
+	
+}
