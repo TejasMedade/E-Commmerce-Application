fd5b5df3072b5301f010a69d1e0b1a4ca63fa3d0
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-02-12 12:12:57 +0530

    Update CustomerDetailsResponseDto.java

--- E_Commerce/src/main/java/com/masai/modelResponseDto/CustomerDetailsResponseDto.java
+++ E_Commerce/src/main/java/com/masai/modelResponseDto/CustomerDetailsResponseDto.java
@@ -1,11 +1,16 @@
 package com.masai.modelResponseDto;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import com.masai.model.Image;
 
 /**
  * 
  */
-
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,21 +23,25 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CustomerDetailsResponseDto {
+public class CustomerDetailsResponseDto extends RepresentationModel<CustomerDetailsResponseDto> {
 
-	private Integer customerId;
-	
+	private Integer userId;
+
 	private String firstName;
-	
-	private String lastName;
-	
-	private String contact;
-	
-	private String email;
-	
-	private LocalDate dateOfBirth;
-	
-	private LocalDate accountCreatedDate;
-	
-}
 
+	private String lastName;
+
+	private String contact;
+
+	private String email;
+
+	private LocalDate dateOfBirth;
+
+	private Image image;
+
+	private LocalDateTime accountCreatedDate;
+
+	private LocalDateTime accountUpdatedDate;
+
+	private Set<RoleResponseDto> roles;
+}
