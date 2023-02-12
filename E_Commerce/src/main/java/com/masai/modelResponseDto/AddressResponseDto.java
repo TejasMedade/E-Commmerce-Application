7518f9df8b12123368c72224b62c1d78c236cb1f
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-02-12 12:12:37 +0530

    Update AddressResponseDto.java

--- E_Commerce/src/main/java/com/masai/modelResponseDto/AddressResponseDto.java
+++ E_Commerce/src/main/java/com/masai/modelResponseDto/AddressResponseDto.java
@@ -3,6 +3,10 @@
  */
 package com.masai.modelResponseDto;
 
+import java.time.LocalDateTime;
+
+import org.springframework.hateoas.RepresentationModel;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,24 +19,27 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AddressResponseDto {
+public class AddressResponseDto extends RepresentationModel<AddressResponseDto> {
 
-private Integer addressId;
-	
+	private Integer addressId;
+
 	private String addressLine1;
-	
+
 	private String addressLine2;
-	
+
 	private String buildingName;
 
 	private String landMark;
-	
+
 	private String city;
-	
+
 	private String state;
-	
+
 	private String country;
-	
+
 	private String pincode;
 
+	private LocalDateTime addressCreatedDate;
+
+	private LocalDateTime addressUpdatedDate;
 }
