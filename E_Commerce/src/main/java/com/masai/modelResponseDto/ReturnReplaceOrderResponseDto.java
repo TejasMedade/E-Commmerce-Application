ff39b8db3d563373cdf70d9fea8e73a4c278e27a
Tejas Vilas Medade <105907169+TejasMedade@users.noreply.github.com>
2023-02-12 12:13:30 +0530

    Create ReturnReplaceOrderResponseDto.java

---
+++ E_Commerce/src/main/java/com/masai/modelResponseDto/ReturnReplaceOrderResponseDto.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.masai.modelResponseDto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author tejas
+ *
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReturnReplaceOrderResponseDto {
+
+	private Integer returnOrderRequestId;
+	
+	private Boolean returnForReplacement;
+
+	private LocalDateTime returnTimeStamp;
+	
+	private LocalDateTime returnUpdatedTimeStamp;
+
+	private Boolean isReturnOrderPickedup;
+	
+	private String status;
+
+	private OrderDetailsResponseDto order;
+
+	private CustomerResponseDto customer;
+
+	private PickUpOrderResponseDto pickUpOrderRequest;
+
+	private ReplaceOrderResponseDto replaceOrderRequest;
+
+}
